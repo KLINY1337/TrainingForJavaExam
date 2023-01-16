a38dd53d73c1f5cfeb171869c602127c5aaea4cf
KLINY1337 <semyon.chernomurov@gmail.com>
2023-01-16 20:09:28 +0300

    Билет 15 (Напишите метод splitStack, который принимает стек целых чисел...)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    //Билет 15
+    public static Stack<Integer> splitStack(Stack<Integer> stack){
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        while(!stack.isEmpty()){
+            queue.add(stack.pop());
+        }
+
+        while (!queue.isEmpty()){
+            int negativeCounter = 0;
+            for (int el : queue){
+                if (el<0) negativeCounter++;
+            }
+            if (negativeCounter > 0){
+                if (queue.peek() < 0) stack.push(queue.remove());
+                else{
+                    queue.add(queue.remove());
+                }
+            }
+            else{
+                while (!queue.isEmpty()){
+                    stack.push(queue.remove());
+                }
+            }
+        }
+        return stack;
     }
 }
